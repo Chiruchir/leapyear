e08f8eac61b07440019573bef733ba1558f8cfd9
Chiruchir <115409561+Chiruchir@users.noreply.github.com>
2023-08-19 15:20:11 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,37 @@
+public class LeapYear 
+{
+    static void isLeapYear(int year)
+    {
+        boolean is_leap_year=false;
+        if(year%4 == 0)
+        {
+        is_leap_year=true;
+        if(year % 100 == 0)
+        {
+            if(year%400 == 0)
+            {
+                is_leap_year=true;
+            }
+            else
+            {
+                is_leap_year=false;
+            }
+        }
+        }
+        else{
+            is_leap_year=false;
+        }
+        if(!is_leap_year)
+        {
+            System.out.println(year+"is not a leap year");
+        }
+        else{
+            System.out.println(year+"is a leap year");
+        }
+    }
+    public static void main(String args[])
+    {
+        isLeapYear(2000);
+        isLeapYear(2002);
+    }
+}
